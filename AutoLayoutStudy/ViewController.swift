55e931894e444f4bd99e66002891214f3edfba8b
Jun <fixme1537@gmail.com>
2023-01-10 22:52:36 +0900

    AutoLayoutStudy..

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -8,11 +8,107 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let labelWidth: CGFloat = 70
 
+    private let mainImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 75
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    // 정렬을 깔끔하게 하기 위한 컨테이너뷰
+    lazy var imageContainView: UIView = {
+        let view = UIView()
+        view.addSubview(mainImageView)
+        //view.backgroundColor = .yellow
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let memberIdLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "멤버번호:"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let memberIdTextField: UITextField = {
+        let tf = UITextField()
+        tf.frame.size.height = 22
+        tf.textColor = .black
+        tf.borderStyle = .roundedRect
+        tf.autocapitalizationType = .none
+        tf.autocorrectionType = .no
+        tf.spellCheckingType = .no
+        tf.clearsOnBeginEditing = false
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+
+    lazy var memberIdStackView: UIStackView = {
+        let stview = UIStackView(arrangedSubviews: [memberIdLabel, memberIdTextField])
+        stview.spacing = 5
+        stview.axis = .horizontal
+        stview.distribution = .fill
+        stview.alignment = .fill
+        stview.translatesAutoresizingMaskIntoConstraints = false
+        return stview
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stview = UIStackView(arrangedSubviews: [imageContainView,memberIdStackView
+                                                    // nameStackView, ageStackView, phoneNumberStackView, addressStackView, saveButton
+                                                   ])
+        stview.spacing = 10
+        stview.axis = .vertical
+        stview.distribution = .fill
+        stview.alignment = .fill
+        stview.translatesAutoresizingMaskIntoConstraints = false
+        return stview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAutoLayout()
         // Do any additional setup after loading the view.
     }
+    
+    private func setupAutoLayout() {
+        view.addSubview(imageContainView)
+        view.addSubview(stackView) // 이런 몽총이~
+        
+        NSLayoutConstraint.activate([
+            mainImageView.heightAnchor.constraint(equalToConstant: 150),
+            mainImageView.widthAnchor.constraint(equalToConstant: 150),
+            mainImageView.centerXAnchor.constraint(equalTo: imageContainView.centerXAnchor),
+            mainImageView.centerYAnchor.constraint(equalTo: imageContainView.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+//            imageContainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            imageContainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            // 이게 있으면 에러가 나네.. 
+            imageContainView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memberIdLabel.widthAnchor.constraint(equalToConstant: labelWidth),
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+        
+        
+        }
 
 
 }
